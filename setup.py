55f10e79ae7e4e9177289e5cc10e8a9fc1f3fc2b
Cano-Muniz, Santiago <tisalon@outlook.com>
2025-03-25 14:57:33 +0100

    add setup.py for package configuration and update pyproject.toml for version and license changes

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hitmictools",
+    version="0.1.2",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+    author="Santiago Cano-Muñiz",
+    author_email="santiago.cano-muniz@unibas.ch",
+    url="https://github.com/phisanti/HiTMicTools",
+    license="EUPL-1.2",
+    entry_points={
+        "console_scripts": [
+            "hitmictools=HiTMicTools.main:main",
+        ],
+    },
+    install_requires=[
+        "torch",
+        "monai",
+        "numpy",
+        "ome-types",
+        "pandas",
+        "scikit-image",
+        "scipy",
+        "pyyaml",
+        "nd2",
+        "tifffile",
+        "jax==0.4.23",
+        "jaxlib==0.4.23",
+        "basicpy @ git+https://github.com/yuliu96/BaSiCPy.git",
+        "jetraw-tools @ git+https://github.com/phisanti/jetraw_tools.git",
+        "onnxruntime",
+        "skl2onnx",
+    ],
+    keywords=["microscopy", "image-analysis", "deep-learning", "cell-segmentation"]
+)
