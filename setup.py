d820a5ec555845f66a5ae30cc497efe833cbc995
Cano-Muniz, Santiago <tisalon@outlook.com>
2025-05-07 13:45:11 +0200

    chore: bump version to 0.3.2 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hitmictools",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     long_description=open("README.md").read(),
