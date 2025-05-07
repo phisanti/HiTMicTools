fbe74f622102a3fc3180ed4d655ef0f44bc8031c
Cano-Muniz, Santiago <tisalon@outlook.com>
2025-05-07 10:50:57 +0200

    feat: bump version to 0.3.1
    This commit updates the package version from 0.3.0 to 0.3.1 in `setup.py`.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hitmictools",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     long_description=open("README.md").read(),
@@ -35,5 +35,5 @@ setup(
         "onnxruntime",
         "skl2onnx",
     ],
-    keywords=["microscopy", "image-analysis", "deep-learning", "cell-segmentation"]
+    keywords=["microscopy", "image-analysis", "deep-learning", "cell-segmentation"],
 )
