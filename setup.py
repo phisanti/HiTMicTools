ae40f558cfb3ed5df959a3272f128f6ea9f9d7cd
Cano-Muniz, Santiago <tisalon@outlook.com>
2025-05-30 11:30:17 +0200

    chore: bump version to 0.3.6
    This commit updates the version of the hitmictools package to 0.3.6 in both `setup.py` and `pyproject.toml`.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hitmictools",
-    version="0.3.5",
+    version="0.3.6",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     long_description=open("README.md").read(),
